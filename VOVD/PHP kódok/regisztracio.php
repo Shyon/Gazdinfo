ffc71e7351b2c2bf84b5b0f44c123dcbd2604de0
Dave525d <szucs.david94@gmail.com>
2014-11-20 12:47:07 +0100

    .

--- VOVD/PHP kódok/regisztracio.php
+++ VOVD/PHP kódok/regisztracio.php
@@ -8,9 +8,9 @@ $dbname="Webkereses";
 mysql_select_db($dbname);
 if($_POST['nev']!="" && $_POST['felhasznalo']!="" && $_POST['jelszo']!="" && $_POST['email']!="")
 {
-$query="insert into users (nev, felhasznalo, jelszo, email) values ('".$_POST['nev']."','".$_POST['felhasznalo']."','".$_POST['jelszo']."','".$_POST['email']."')";
+$query="insert into Felhasznalo (nev, felhasznalo, jelszo, email) values ('".$_POST['nev']."','".$_POST['felhasznalo']."','".$_POST['jelszo']."','".$_POST['email']."')";
 mysql_query($query) or die ("Nem sikerult"." ".$query);
 }
 mysql_close($con);
-echo '<meta http-equiv="refresh" content="0; URL=belepoellenorzes.php">';
+echo '<meta http-equiv="refresh" content="0; URL=belepes.php">';
 ?>
