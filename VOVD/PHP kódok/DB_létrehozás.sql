8eb22f62a6a9c3df8f8a2a663477abcaf069c605
shoii <mulyad.shoia@gmail.com>
2014-11-20 12:28:18 +0100

    .

---
+++ VOVD/PHP kódok/DB_létrehozás.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS `Webkereses` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `Webkereses`;
+
+CREATE TABLE IF NOT EXISTS `Felhasznalo` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nev` varchar(50) NOT NULL,
+  `felhasznalo` varchar(50) NOT NULL,
+  `jelszo` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+)
+
+CREATE TABLE IF NOT EXISTS `Kereses` (
+  `felhasznalo` varchar(50) NOT NULL,
+  `url` varchar(50) NOT NULL,
+  `kerszov` varchar(30) NOT NULL,
+  `datum` date
+  PRIMARY KEY (`)
+)
